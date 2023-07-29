ce7dd81dd58ac1ea079186b6defd27c48e70884f
PlayerG9 <g9yout@gmail.com>
2023-07-29 11:50:59 +0200

    added loggin to register.* functions

--- src/interceptor/register.py
+++ src/interceptor/register.py
@@ -3,6 +3,7 @@
 r"""
 
 """
+import logging
 
 
 BEFORE = []
@@ -11,12 +12,15 @@ TEARDOWN = []
 
 
 def before_request(fn):
+    logging.info(f"register.before_request({fn.__name__})")
     BEFORE.append(fn)
 
 
 def after_response(fn):
+    logging.info(f"register.after_response({fn.__name__})")
     AFTER.append(fn)
 
 
 def teardown_request(fn):
+    logging.info(f"register.teardown_request({fn.__name__})")
     TEARDOWN.append(fn)
