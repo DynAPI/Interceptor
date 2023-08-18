e45fcd42d1050f45cfc2d8e817aed8c899be879e
PlayerG9 <g9yout@gmail.com>
2023-08-18 10:17:35 +0200

    added support for websockets and code reformat + slight bug fixes

--- src/interceptor/interceptorhandler.py
+++ src/interceptor/interceptorhandler.py
@@ -3,16 +3,15 @@
 r"""
 
 """
-import sys
+import socket
 import logging
-import traceback
 import http.server
 import http.client
 
 from config import config
 from classes import Request, Response
 import register
-from exceptions import HTTPException
+from extra_exceptions import HTTPException
 
 
 THOST, TPORT = config.get("target", "host"), config.getint("target", "port")
@@ -20,8 +19,10 @@ TIMEOUT = config.getfloat("target", "timeout")
 
 
 class InterceptorHandler(http.server.BaseHTTPRequestHandler):
-    request: Request = None
-    response: Response = None
+    x_request: Request = None
+    x_response: Response = None
+    x_final_response: Response = None
+    x_connection: http.client.HTTPConnection = None
 
     def __getattr__(self, item: str):
         if item.startswith("do_"):
@@ -31,51 +32,63 @@ class InterceptorHandler(http.server.BaseHTTPRequestHandler):
     def _handle_wrapper(self):
         try:
             self._handle_request()
+        except ConnectionRefusedError:
+            self._handle_http_exception(
+                HTTPException(
+                    status=http.HTTPStatus.SERVICE_UNAVAILABLE
+                )
+            )
         except HTTPException as exc:
             self._handle_http_exception(exc)
         except Exception as exc:
-            traceback.print_exception(type(exc), exc, exc.__traceback__)
+            logging.error("_handle_request failed", exc_info=exc)
             self._handle_http_exception(
                 HTTPException(
-                    status=500,
+                    status=http.HTTPStatus.INTERNAL_SERVER_ERROR,
                     message=f"{exc}"
                 )
             )
         finally:
             for handler in register.TEARDOWN:
                 try:
-                    handler(self.request, self.response)
+                    self._call_handler(handler)
                 except Exception as exc:
-                    print(f"teardown handler '{handler.__name__}' failed", file=sys.stderr)
-                    traceback.print_exception(type(exc), exc, exc.__traceback__)
+                    logging.error(f"teardown handler '{handler.__name__}' failed", exc_info=exc)
 
     def _handle_request(self):
         logging.info(f"Handle Request from {self.client_address[0]}")
         method = self.command
-        logging.debug("method:", method)
+        logging.debug(f"method: {method}")
         path = self.path
-        logging.debug("path:", path)
+        logging.debug(f"path: {path}")
         headers = self.headers
         forwarded_for = headers.get("X-Forwarded-For")
         if forwarded_for is None:
             headers.add_header("X-Forwarded-For", self.client_address[0])
         else:
             headers.replace_header("X-Forwarded-For", f"{forwarded_for}, {self.client_address[0]}")
-        logging.debug("headers:", headers)
+        logging.debug(f"headers: {headers}")
         content_length = int(headers.get("Content-Length", 0))
         body = self.rfile.read(content_length) if content_length else None
-        logging.debug("body:", body and body[:20])
+        logging.debug(f"body: {body and body[:20]}")
 
-        # client_address is localhost if behind neginx or apache2
-        self.request = request = Request(client=self.client_address[0], method=method, path=path, headers=headers, body=body)
+        # client_address is localhost if behind nginx or apache2
+        self.x_request = Request(client=self.client_address[0], method=method, path=path, headers=headers, body=body)
 
         logging.info("running interceptors (before)")
         for interceptor in register.BEFORE:
-            early_response = interceptor(request)
+            logging.debug(f"(before) Interceptor: {interceptor.__name__}")
+            early_response = self._call_handler(interceptor)
             if early_response is not None:
                 logging.info(f"Early Response from {interceptor.__name__}")
                 self._handle_response(early_response)
                 return
+
+        if headers.get("Upgrade", None) == "websocket":
+            logging.info("Upgrading to websocket")
+            self._handle_websocket_handshake()
+            self._handle_websocket_connection()
+            return
 
         logging.debug("Building Connection...")
         connection = http.client.HTTPConnection(THOST, TPORT, timeout=TIMEOUT)
@@ -87,11 +100,12 @@ class InterceptorHandler(http.server.BaseHTTPRequestHandler):
             headers={k: v for k, v in headers.raw_items()}
         )
         logging.debug("Waiting for response...")
-        response = Response.from_http_response(connection.getresponse())
+        self.x_response = response = Response.from_http_response(connection.getresponse())
 
         logging.info("running interceptors (after)")
         for interceptor in register.AFTER:
-            early_response = interceptor(request, response)
+            logging.debug(f"(after) Interceptor: {interceptor.__name__}")
+            early_response = self._call_handler(interceptor)
             if early_response is not None:
                 logging.info(f"Early Response from {interceptor.__name__}")
                 self._handle_response(early_response)
@@ -100,7 +114,7 @@ class InterceptorHandler(http.server.BaseHTTPRequestHandler):
         self._handle_response(response)
 
     def _handle_response(self, response: Response):
-        self.response = response
+        self.x_final_response = response
         logging.debug("sending response...")
         self.send_response_only(response.status)
         logging.debug("sending headers...")
@@ -118,3 +132,63 @@ class InterceptorHandler(http.server.BaseHTTPRequestHandler):
         self._handle_response(
             Response.from_http_exception(exc)
         )
+
+    def _call_handler(self, handler):
+        if not hasattr(handler, '__handler_args__'):
+            import inspect
+            sig = inspect.signature(handler)
+            handler.__handler_args__ = list(sig.parameters.keys())
+        args = dict(
+            self=self,
+            request=self.x_request,
+            response=self.x_response,
+            final_response=self.x_final_response,
+            connection=self.x_connection,
+        )
+        args['kwargs'] = args
+        return handler(**{arg: args.get(arg, None) for arg in handler.__handler_args__})
+
+    # Websocket-Implementation
+
+    def _handle_websocket_handshake(self):
+        logging.debug("WebSocket Handshake")
+        request = self.x_request
+        self.x_connection = conn = http.client.HTTPConnection(THOST, TPORT, timeout=TIMEOUT)
+        self.x_connection.request(
+            method=request.method,
+            url=request.path,
+            body=request.body,
+            headers=request.headers,
+        )
+        response = conn.getresponse()
+        self.send_response_only(response.status)
+        for name, value in response.headers.raw_items():
+            self.send_header(name, value)
+        self.end_headers()
+
+    def _handle_websocket_connection(self):
+        import select
+
+        client: socket.socket = self.connection
+        server: socket.socket = self.x_connection.sock
+
+        alive = True
+
+        logging.debug("WebSocket Loop")
+        while alive:
+            read_sockets, write_sockets, error_sockets = select.select(
+                [client, server], [], []
+            )
+
+            for reader in read_sockets:
+                reader: socket.socket
+                # print(reader == client, reader == server)
+                sender = server if reader is client else client
+                data = reader.recv(1024)
+                if not data:
+                    alive = False
+                    break
+                logging.debug(f"ws {'<' if reader is client else '>'} {data}")
+                sender.sendall(data)
+
+        logging.debug("WebSocket completed")
